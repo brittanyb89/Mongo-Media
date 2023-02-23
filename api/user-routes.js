a2bfc04e38efc542db4e8a337461fb19d2afb87e
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-02-23 15:10:15 -0600

    Create user routes

---
+++ api/user-routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+
+// Set requirements from users-controller.js
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/users-controller");
+
+// Set up <GET, POST> at /api/users
+router.route("/").get(getAllUsers).post(createUser);
+
+// Set up <GET(single), PUT, DELETE> at /api/users/:id
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// Set up <POST, DELETE> at /api/users/:Id/friends/:friendId
+router.route("/:Id/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+// Export the router
+module.exports = router;
