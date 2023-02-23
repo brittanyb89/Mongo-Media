978905910d1618a6e1357d18b5a8e0428a9f4e68
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-02-23 14:29:21 -0600

    Create user model

---
+++ models/users.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      // RegEx for email validation
+      match: [
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+        "Please enter a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Get total count of friends on retrieval
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// Create the User model using the UserSchema
+const User = model("User", userSchema);
+
+module.exports = User;
